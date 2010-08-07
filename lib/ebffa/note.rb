665d2519e19a6e1281b823f73470d3d118580623
Jeremy Ruten <jeremy.ruten@gmail.com>
2010-08-06 21:43:58 -0600

    Add Note#succ and fix for Ruby 1.9

--- lib/ebffa/note.rb
+++ lib/ebffa/note.rb
@@ -39,6 +39,11 @@ module EbFFA
       @semitones <=> other.semitones
     end
 
+    # Returns the Note one semitone above this one.
+    def succ
+      (self ^ Mn2).simplify_name
+    end
+
     # The triple-equals tells you if two notes are the same, regardless of what
     # octave they're in. Basically, if the two notes *can* be designated the
     # same name (since name isn't specific to an octave), they are triply-equal.
@@ -49,6 +54,14 @@ module EbFFA
     # Raises or lowers a note to octave 4 (the good and truly upright octave).
     def base
       Note.new(@letter, @accidental)
+    end
+
+    # Returns a new Note equal to this one, but with either a natural or a sharp
+    # for an accidental.
+    def simplify_name
+      Note.new(_semitones_to_letter(@semitones),
+               _semitones_to_accidental(@semitones),
+               _semitones_to_octave(@semitones))
     end
 
     # Collect two notes into a Chord.
@@ -129,23 +142,31 @@ module EbFFA
 
     def _add_note_letter(letter, interval)
       interval += interval > 0 ? -1 : 1
-      %w{ A B C D E F G }[(letter.to_s.upcase[0] - ?A + interval) % 7]
+      %w[A B C D E F G][(letter.to_s.upcase.ord - "A".ord + interval) % 7]
     end
 
     def _letter_to_semitones(letter)
       { :C => 0, :D => 2, :E => 4, :F => 5, :G => 7, :A => 9, :B => 11 }[letter.to_s.upcase.to_sym]
     end
 
+    def _semitones_to_letter(semitones)
+      %w[C C D D E F F G G A A B][semitones % 12].to_sym
+    end
+
     def _accidental_to_semitones(accidental)
       { :bb => -2, :b => -1, :n => 0, :s => 1, :ss => 2 }[accidental.to_s.downcase.to_sym]
     end
 
-    def _semitones_to_octave(semitones)
-      semitones / 12 + 4
+    def _semitones_to_accidental(semitones)
+      %w[n s n s n n s n s n s n][semitones % 12].to_sym
     end
 
     def _octave_to_semitones(octave)
       12 * (octave - 4)
+    end
+
+    def _semitones_to_octave(semitones)
+      semitones / 12 + 4
     end
   end
 end
